1f94d0190dc09463f23fdd26b05d7054206ef079
constantin.cheptea <cheptea.constantin@gmail.com>
2020-05-15 18:40:46 +0300

    Modify the static plug.

--- lib/phoenix_angular_web/endpoint.ex
+++ lib/phoenix_angular_web/endpoint.ex
@@ -22,7 +22,7 @@ defmodule PhoenixAngularWeb.Endpoint do
     at: "/",
     from: :phoenix_angular,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt frontend)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
